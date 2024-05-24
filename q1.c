34d2eee7d6322273a07eff8c29d4fc5e06d2e744
roudasaffuri <saffuri87@gmail.com>
2024-05-24 18:09:42 +0300

    fix

--- q1.c
+++ q1.c
@@ -6,18 +6,13 @@ int main() {
     scanf("%d", &number);
 
     printf("Number     Square      Cube\n");
-    if(number<0){
+   
      for (int i = 0; i <=4 ; i++) {
     printf("%d          %d           %d\n", number,  number * number, number * number * number);
      number++;
      }
-    }
-     else  {
-         for (int i = 0; i <=4 ; i++) {
-    printf("%d          %d           %d\n", number,  number * number, number * number * number);
-     number++;
-     }
-     }
+    
+     
 
     return 0;
 }
